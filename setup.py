b38f037062b151f0509c0a871f5f7b34fb872e86
Arno Candel <arno.candel@gmail.com>
2017-04-12 18:28:27 -0700

    Rename lib dir into glmnet to allow package loading in python

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='glmnet_python',
+setup(name='glmnet',
 	version = '0.1',
 	description = 'Python version of GLMnet, Stanford University',
 	url = '',
 	author = '',
 	author_email = 'bbalasub@stanford.edu',
 	license = 'GPL-2',
-	packages=['lib'],
+	packages=['glmnet'],
 	zip_safe = False)
 
