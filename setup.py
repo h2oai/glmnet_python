88c6064cf3a1b250e91abe24d74e97e0566fcf26
Arno Candel <arno.candel@gmail.com>
2017-04-12 18:44:28 -0700

    Revert "Rename lib dir into glmnet to allow package loading in python"
    This reverts commit b38f037062b151f0509c0a871f5f7b34fb872e86.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='glmnet',
+setup(name='glmnet_python',
 	version = '0.1',
 	description = 'Python version of GLMnet, Stanford University',
 	url = '',
 	author = '',
 	author_email = 'bbalasub@stanford.edu',
 	license = 'GPL-2',
-	packages=['glmnet'],
+	packages=['lib'],
 	zip_safe = False)
 
